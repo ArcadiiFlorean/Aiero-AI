00115f9a88cebca0f28598d551cf765c28002ab1
Arcadii Florean <arcadiiflorean789@gmail.com>
2025-02-13 19:18:01 +0000

    faq section

---
+++ script.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('.faq-question').forEach(item => {
+    item.addEventListener('click', () => {
+        const answer = item.nextElementSibling;
+        
+        // Verifică dacă răspunsul este deja deschis
+        if (answer.style.display === "block") {
+            answer.style.display = "none";
+        } else {
+            // Închide toate răspunsurile deschise
+            document.querySelectorAll('.faq-answer').forEach(ans => {
+                ans.style.display = "none";
+            });
+            // Deschide răspunsul pentru întrebarea curentă
+            answer.style.display = "block";
+        }
+    });
+});
